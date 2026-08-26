19fd3d70edb0c2f101d53701c048607ca34ab737
emdguv <emd149@georgetown.edu>
2026-08-26 14:30:27 -0400

    add counter

--- test1.py
+++ test1.py
@@ -1,3 +1,8 @@
-for i in range(10):
-    for j in range(10):
+counter = 0
+
+for i in range(100):
+    for j in range(4):
         print(i % (j+1))
+        counter += 1
+        print("This is loop " + str(counter))
+
